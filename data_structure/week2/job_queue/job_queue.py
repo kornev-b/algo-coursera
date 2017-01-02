64f5c1cb84ccc754b6de4042c148dba56c6eea7c
Bogdan Kornev <bkornev@swiftcom.uk>
2017-01-02 20:14:00 +0600

    job queue

--- data_structure/week2/job_queue/job_queue.py
+++ data_structure/week2/job_queue/job_queue.py
@@ -50,12 +50,18 @@ def siftDown(a, i):
     while left_child(i) < len(a):
         min_i = i
         l = left_child(i)
-        if (l < len(a) and (a[l][1] < a[min_i][1])) or (
-                        a[l][1] == a[min_i][1] and a[l][0] < a[min_i][0]):
+        if (l < len(a) and (a[l][1] < a[min_i][1])) \
+                or (l < len(a) and a[l][1] == a[min_i][1] and a[l][0] < a[min_i][0]
+                    and a[min_i][1] + time[a[min_i][0]] == a[l][1] + time[a[l][0]]) \
+                or (l < len(a) and a[min_i][1] + time[a[min_i][0]] > a[l][1] + time[a[l][0]]) \
+                or (l < len(a) and counts[a[min_i][0]] > counts[a[l][0]]):
             min_i = l
         r = right_child(i)
-        if (r < len(a) and (a[r][1] < a[min_i][1])) or (
-                            r < len(a) and a[r][1] == a[min_i][1] and a[r][0] < a[min_i][0]):
+        if (r < len(a) and (a[r][1] < a[min_i][1])) \
+                or (r < len(a) and a[r][1] == a[min_i][1] and a[r][0] < a[min_i][0]
+                    and a[min_i][1] + time[a[min_i][0]] == a[r][1] + time[a[r][0]]) \
+                or (r < len(a) and a[min_i][1] + time[a[min_i][0]] > a[r][1] + time[a[r][0]]) \
+                or (r < len(a) and counts[a[min_i][0]] > counts[a[r][0]]):
             min_i = r
         if i != min_i:
             a[i], a[min_i] = a[min_i], a[i]
@@ -67,7 +73,7 @@ def siftDown(a, i):
 def siftUp(a, i):
     while i > 0:
         p = parent(i)
-        if (a[p][1] > a[i][1]) or (a[p][0] == a[i][0] and a[p][1] > a[i][1]):
+        if (a[p][1] > a[i][1]):  # or (a[p][1] == a[i][1] and a[p][0] > a[i][0]):
             a[i], a[p] = a[p], a[i]
         i = p
 
@@ -91,32 +97,55 @@ def pop(a):
     return max
 
 
+with open('tests\\02') as f:
+    content = f.read().splitlines()
+num_workers, m = map(int, content[0].split())
+jobs = map(int, content[1].split())
+
 # num_workers, m = map(int, input().split())
 # jobs = list(map(int, input().split()))
 
-num_workers = 4
+# num_workers = 4
 # m = 20
 tasks = []
 # jobs = [10000,1000,1000,1000,1000]
 # jobs = [1, 2, 3, 4, 5]
 # jobs = [1, 1, 1, 1, 1]
 # jobs = [1000000000, 1, 1, 1, 1, 1, 1, 1, 1]
-jobs = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+# jobs = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 # jobs = [1, 1]
+# jobs = [0, 0, 0, 0, 0, 0, 0, 0]
+# jobs = [1, 2, 3]
 a = []
 time = []
+counts = []
 for i in range(0, num_workers):
     if i == len(jobs):
         break
     insert(a, (i, jobs[i]))
-    time.append(0)
+    tasks.append((i, 0))
+    time.append(jobs[i])
+    counts.append(0)
 i = num_workers
 while len(a) > 0:
     free = pop(a)
-    tasks.append((free[0], time[free[0]]))
+    t = time[free[0]]
     if i < len(jobs):
-        insert(a, (free[0], jobs[i]))
-    time[free[0]] += free[1]
+        insert(a, (free[0], free[1] + jobs[i]))
+        tasks.append((free[0], t))
+        time[free[0]] += jobs[i]
     i += 1
+    counts[free[0]] += 1
 for i in range(0, len(tasks)):
-    print(tasks[i][0], tasks[i][1])
+    print(i, tasks[i][0], tasks[i][1])
+
+a = []
+with open('tests\\02.a') as f:
+    for line in f:
+        int_list = [int(i) for i in line.split()]
+        a.append(int_list)
+
+for i in range(0, len(tasks)):
+    assert tasks[i][0] == a[i][0] and tasks[i][1] == a[i][1], '{} {} != {} {}, index = {}'.format(tasks[i][0],
+                                                                                                    tasks[i][1],
+                                                                                                    a[i][0], a[i][1], i)
